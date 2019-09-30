ed093a62597a4e2e13df485cce11fc778082f916
Peter Pistorius <peter.pistorius@gmail.com>
2019-09-30 10:05:17 +0200

    Use a workspace package for photon.

---
+++ api/scripts/prisma-default-env-vars.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This file exists because it's difficult to provide configurable
+# defaults to Prisma.
+#
+# But, why?
+#
+# 1. Prisma reads .env vars where the command is run
+# 2. Prisma does not allow the datamodel's path to be configured
+# 3. As a result our prisma2 command is run in `./api/` and not in `./`
+#    where our command resides
+# 4. Prisma's `enabled()` flag doesn't work.
+
+if [[ -z "${PRISMA_PROVIDER}" ]]; then
+  PRISMA_PROVIDER="sqlite"
+fi
+
+if [[ -z "${PRISMA_URL}" ]]; then
+  PRISMA_URL="file:./dev.db"
+fi
+
+echo "Prisma Provider: ${PRISMA_PROVIDER}"
+echo "Prisma URL     : ${PRISMA_URL}"
+
+export PRISMA_URL=$PRISMA_URL; export PRISMA_PROVIDER=$PRISMA_PROVIDER; yarn prisma2 $@
---
+++ packages/photon/README.md
@@ -0,0 +1,4 @@
+# Why is this here?
+
+This `@generated/photon` package exists because we need to reference
+photon as an actual depenedency to have the files bundled properly during packaging and distribution to production.
