54299e5bbc2234eb32d8f10d91909c876de683a0
Peter Pistorius <peter.pistorius@gmail.com>
2020-01-29 19:49:48 +0100

    Use code that auto maps services.

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -1,15 +1,12 @@
 import importAll from '@redwoodjs/core/dist/importAll.macro'
-import { server, makeMergedSchema } from '@redwoodjs/api'
-import { Photon } from '@prisma/photon'
+import { server, makeMergedSchema, makeServices } from '@redwoodjs/api'
 
-// Collect all the GraphQL schema definitions and resolvers.
-const specs = importAll('api', 'graphql')
-const specArray = Object.keys(specs).map((file) => specs[file])
-const schema = makeMergedSchema(specArray)
-
-const photon = new Photon()
+const schemas = importAll('api', 'graphql')
+const services = importAll('api', 'services')
 
 export const handler = server({
-  schema,
-  context: { photon },
+  schema: makeMergedSchema({
+    schemas,
+    services: makeServices({ services }),
+  }),
 }).createHandler()
