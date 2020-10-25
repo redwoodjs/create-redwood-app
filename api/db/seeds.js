09069096fb9b0e92679fc69836c57e64186f4c61
Davy Hausser <davy.hausser@gmail.com>
2020-10-25 22:36:00 +0100

    🚚 Renamed `api/prisma` to `api/db`

--- api/db/seeds.js
+++ api/db/seeds.js
@@ -16,7 +16,7 @@ async function main() {
   //     await db.user.create({ data: { name: 'Admin', email: 'admin@email.com' }})
   //   }
 
-  console.info('No data to seed. See api/prisma/seeds.js for info.')
+  console.info('No data to seed. See api/db/seeds.js for info.')
 }
 
 main()
